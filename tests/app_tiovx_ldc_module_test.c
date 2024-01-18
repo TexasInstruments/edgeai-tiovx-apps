5ae99e420ac79ad056d1595849bfbf0dab88378d
Rahul T R <r-ravikumar@ti.com>
2024-01-18 21:08:22 +0530

    modules: Add LDC module and test
    Signed-off-by: Rahul T R <r-ravikumar@ti.com>

--- tests/app_tiovx_ldc_module_test.c
+++ tests/app_tiovx_ldc_module_test.c
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright (c) 2017 Texas Instruments Incorporated
+ * Copyright (c) 2021 Texas Instruments Incorporated
  *
  * All rights reserved not granted herein.
  *
@@ -60,85 +60,95 @@
  *
  */
 
-#include <stdio.h>
-#include <TI/tivx.h>
-#include <app_init.h>
-#include <stdlib.h>
 #include <tiovx_modules.h>
+#include <tiovx_utils.h>
 
-#define APP_MODULES_TEST_COLOR_CONVERT (1)
-#define APP_MODULES_TEST_DL_COLOR_CONVERT (1)
-#define APP_MODULES_TEST_MULTI_SCALER (1)
-#define APP_MODULES_TEST_VISS (1)
-#define APP_MODULES_TEST_LDC (1)
+#define APP_BUFQ_DEPTH   (1)
+#define APP_NUM_CH       (1)
 
-char *EDGEAI_DATA_PATH;
+#define INPUT_WIDTH  (1936)
+#define INPUT_HEIGHT (1096)
 
-int main(int argc, char *argv[])
+#define OUTPUT_WIDTH  (1920)
+#define OUTPUT_HEIGHT (1080)
+
+#define LDC_TABLE_WIDTH     (1920)
+#define LDC_TABLE_HEIGHT    (1080)
+#define LDC_DS_FACTOR       (2)
+#define LDC_BLOCK_WIDTH     (64)
+#define LDC_BLOCK_HEIGHT    (32)
+#define LDC_PIXEL_PAD       (1)
+
+#define SENSOR_NAME "SENSOR_SONY_IMX390_UB953_D3"
+#define DCC_LDC "/opt/imaging/imx390/wdr/dcc_ldc_wdr.bin"
+
+vx_status app_modules_ldc_test(vx_int32 argc, vx_char* argv[])
 {
-    int status = 0;
+    vx_status status = VX_FAILURE;
+    GraphObj graph;
+    NodeObj *node = NULL;
+    TIOVXLdcNodeCfg cfg;
+    BufPool *in_buf_pool = NULL, *out_buf_pool = NULL;
+    Buf *inbuf = NULL, *outbuf = NULL;
+    char input_filename[100];
+    char output_filename[100];
 
-    EDGEAI_DATA_PATH = getenv("EDGEAI_DATA_PATH");
-    if (EDGEAI_DATA_PATH == NULL)
-    {
-      TIOVX_MODULE_ERROR("EDGEAI_DATA_PATH Not Defined!!\n");
-    }
+    sprintf(input_filename, "%s/raw_images/modules_test/imx390_fisheye_1936x1096_nv12.yuv", EDGEAI_DATA_PATH);
+    sprintf(output_filename, "%s/output/imx390_rectified_1920x1080_nv12.yuv", EDGEAI_DATA_PATH);
 
-    status = appInit();
+    tiovx_ldc_init_cfg(&cfg);
 
-#if (APP_MODULES_TEST_MULTI_SCALER)
-    if(status==0)
-    {
-        printf("Running Multi Scaler module test\n");
-        int app_modules_multi_scaler_test(int argc, char* argv[]);
+    snprintf(cfg.dcc_config_file, TIVX_FILEIO_FILE_PATH_LENGTH, "%s", DCC_LDC);
+    snprintf(cfg.lut_file, TIVX_FILEIO_FILE_PATH_LENGTH,
+            "%s/raw_images/modules_test/imx390_ldc_lut_1920x1080.bin",
+            EDGEAI_DATA_PATH);
 
-        status = app_modules_multi_scaler_test(argc, argv);
-    }
-#endif
+    cfg.ldc_mode = TIOVX_MODULE_LDC_OP_MODE_DCC_DATA; //TIOVX_MODULE_LDC_OP_MODE_MESH_IMAGE
 
-#if (APP_MODULES_TEST_DL_COLOR_CONVERT)
-    if(status==0)
-    {
-        printf("Running DL color convert module test\n");
-        int app_modules_dl_color_convert_test(int argc, char* argv[]);
+    cfg.input_cfg.color_format = VX_DF_IMAGE_NV12;
+    cfg.input_cfg.width = INPUT_WIDTH;
+    cfg.input_cfg.height = INPUT_HEIGHT;
 
-        status = app_modules_dl_color_convert_test(argc, argv);
-    }
-#endif
+    cfg.output_cfgs[0].color_format = VX_DF_IMAGE_NV12;
+    cfg.output_cfgs[0].width = OUTPUT_WIDTH;
+    cfg.output_cfgs[0].height = OUTPUT_HEIGHT;
 
-#if (APP_MODULES_TEST_COLOR_CONVERT)
-    if(status==0)
-    {
-        printf("Running color convert module test\n");
-        int app_modules_color_convert_test(int argc, char* argv[]);
+    cfg.init_x = 0;
+    cfg.init_y = 0;
+    cfg.table_width = LDC_TABLE_WIDTH;
+    cfg.table_height = LDC_TABLE_HEIGHT;
+    cfg.ds_factor = LDC_DS_FACTOR;
+    cfg.out_block_width = LDC_BLOCK_WIDTH;
+    cfg.out_block_height = LDC_BLOCK_HEIGHT;
+    cfg.pixel_pad = LDC_PIXEL_PAD;
 
-        status = app_modules_color_convert_test(argc, argv);
-    }
-#endif
+    sprintf(cfg.target_string, TIVX_TARGET_VPAC_LDC1);
 
-#if (APP_MODULES_TEST_VISS)
-    if(status==0)
-    {
-        printf("Running viss module test\n");
-        int app_modules_viss_test(int argc, char* argv[]);
+    status = tiovx_modules_initialize_graph(&graph);
+    node = tiovx_modules_add_node(&graph, TIOVX_LDC, (void *)&cfg);
+    status = tiovx_modules_verify_graph(&graph);
 
-        status = app_modules_viss_test(argc, argv);
-    }
-#endif
+    in_buf_pool = node->sinks[0].buf_pool;
+    inbuf = tiovx_modules_acquire_buf(in_buf_pool);
+    readImage(input_filename, (vx_image)inbuf->handle);
+    tiovx_modules_enqueue_buf(inbuf);
 
-#if (APP_MODULES_TEST_LDC)
-    if(status==0)
-    {
-        printf("Running ldc module test\n");
-        int app_modules_ldc_test(int argc, char* argv[]);
+    out_buf_pool = node->srcs[0].buf_pool;
+    outbuf = tiovx_modules_acquire_buf(out_buf_pool);
+    tiovx_modules_enqueue_buf(outbuf);
 
-        status = app_modules_ldc_test(argc, argv);
-    }
-#endif
+    tiovx_modules_schedule_graph(&graph);
+    tiovx_modules_wait_graph(&graph);
 
-    printf("All tests complete!\n");
+    inbuf = tiovx_modules_dequeue_buf(in_buf_pool);
+    outbuf = tiovx_modules_dequeue_buf(out_buf_pool);
 
-    appDeInit();
+    writeImage(output_filename, (vx_image)outbuf->handle);
+
+    tiovx_modules_release_buf(inbuf);
+    tiovx_modules_release_buf(outbuf);
+
+    tiovx_modules_clean_graph(&graph);
 
     return status;
 }
