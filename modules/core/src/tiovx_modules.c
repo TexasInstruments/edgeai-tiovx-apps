906fd39e17436fd12273fe871e9c741769f64a1c
Rahul T R <r-ravikumar@ti.com>
2024-02-07 16:29:40 +0530

    modules: core: Load Imaging kernels during graph init
    Signed-off-by: Rahul T R <r-ravikumar@ti.com>

--- modules/core/src/tiovx_modules.c
+++ modules/core/src/tiovx_modules.c
@@ -74,10 +74,16 @@ vx_status tiovx_modules_initialize_graph(GraphObj *graph)
 
     CLR(graph);
     graph->tiovx_context = vxCreateContext();
+
+    tivxHwaLoadKernels(graph->tiovx_context);
+    tivxImgProcLoadKernels(graph->tiovx_context);
     tivxEdgeaiImgProcLoadKernels(graph->tiovx_context);
     tivxTIDLLoadKernels(graph->tiovx_context);
-    tivxHwaLoadKernels(graph->tiovx_context);
     tivxVideoIOLoadKernels(graph->tiovx_context);
+#if !defined (SOC_AM62A)
+    tivxImagingLoadKernels(graph->tiovx_context);
+#endif
+
     graph->tiovx_graph = vxCreateGraph(graph->tiovx_context);
     graph->schedule_mode = VX_GRAPH_SCHEDULE_MODE_QUEUE_MANUAL;
 
@@ -484,10 +490,16 @@ vx_status tiovx_modules_clean_graph(GraphObj *graph)
     }
 
     vxReleaseGraph(&graph->tiovx_graph);
+
+#if !defined (SOC_AM62A)
+    tivxImagingUnLoadKernels(graph->tiovx_context);
+#endif
     tivxVideoIOUnLoadKernels(graph->tiovx_context);
-    tivxHwaUnLoadKernels(graph->tiovx_context);
     tivxTIDLUnLoadKernels(graph->tiovx_context);
     tivxEdgeaiImgProcUnLoadKernels(graph->tiovx_context);
+    tivxImgProcUnLoadKernels(graph->tiovx_context);
+    tivxHwaUnLoadKernels(graph->tiovx_context);
+
     vxReleaseContext(&graph->tiovx_context);
     CLR(graph);
 
