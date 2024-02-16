3b4701955b0c1cd6d79c62ae3bc963c7dbb88017
Rahul T R <r-ravikumar@ti.com>
2024-02-16 13:58:40 +0530

    tests: Add test for linux encode module
    Signed-off-by: Rahul T R <r-ravikumar@ti.com>

--- tests/main.c
+++ tests/main.c
@@ -83,6 +83,7 @@
 #define APP_MODULES_TEST_V4L2_CAPTURE (0)
 #define APP_MODULES_TEST_LINUX_CAPTURE_DISPLAY (0)
 #define APP_MODULES_TEST_LINUX_DECODE_DISPLAY (0)
+#define APP_MODULES_TEST_LINUX_CAPTURE_ENCODE (0)
 #define APP_MODULES_TEST_CAPTURE_VISS_LDC_MSC_DISPLAY (0)
 #define APP_MODULES_TEST_CAPTURE_DL_DISPLAY (0)
 
@@ -230,6 +231,15 @@ int main(int argc, char *argv[])
         int app_modules_linux_decode_display_test(int argc, char* argv[]);
 
         status = app_modules_linux_decode_display_test(argc, argv);
+    }
+#endif
+#if (APP_MODULES_TEST_LINUX_CAPTURE_ENCODE)
+    if(status==0)
+    {
+        printf("Running linux encode module test\n");
+        int app_modules_linux_capture_encode_test(int argc, char* argv[]);
+
+        status = app_modules_linux_capture_encode_test(argc, argv);
     }
 #endif
 
