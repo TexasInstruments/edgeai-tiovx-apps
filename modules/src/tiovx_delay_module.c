0ee57f7417f9df956f3fbb98513fb829edb15c88
Rahul T R <r-ravikumar@ti.com>
2024-02-28 20:30:46 +0530

    modules: Add delay module
    Signed-off-by: Rahul T R <r-ravikumar@ti.com>

--- modules/src/tiovx_delay_module.c
+++ modules/src/tiovx_delay_module.c
@@ -59,65 +59,95 @@
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  */
-#ifndef _TIOVX_MODULES_CBS
-#define _TIOVX_MODULES_CBS
-
-#include "tiovx_multi_scaler_module.h"
-#include "tiovx_dl_color_convert_module.h"
-#include "tiovx_color_convert_module.h"
-#include "tiovx_viss_module.h"
-#include "tiovx_ldc_module.h"
-#include "tiovx_tee_module.h"
-#include "tiovx_tidl_module.h"
-#include "tiovx_dl_pre_proc_module.h"
-#include "tiovx_dl_post_proc_module.h"
-#include "tiovx_mosaic_module.h"
-#include "tiovx_obj_array_split_module.h"
-#include "tiovx_pyramid_module.h"
 #include "tiovx_delay_module.h"
 
-#if defined(SOC_J721E) || defined(SOC_J721S2) || defined(SOC_J784S4)
-#include "tiovx_display_module.h"
-#include "tiovx_capture_module.h"
-#include "tiovx_aewb_module.h"
-#include "tiovx_sde_module.h"
-#include "tiovx_sde_viz_module.h"
-#include "tiovx_dof_module.h"
-#include "tiovx_dof_viz_module.h"
-#endif
+typedef struct {
+    vx_delay delay_obj;
+} TIOVXDelayNodePriv;
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-typedef enum {
-    TIOVX_MULTI_SCALER = 0,
-    TIOVX_DL_COLOR_CONVERT,
-    TIOVX_COLOR_CONVERT,
-    TIOVX_VISS,
-    TIOVX_LDC,
-    TIOVX_TEE,
-    TIOVX_TIDL,
-    TIOVX_DL_PRE_PROC,
-    TIOVX_DL_POST_PROC,
-    TIOVX_MOSAIC,
-    TIOVX_OBJ_ARRAY_SPLIT,
-    TIOVX_PYRAMID,
-    TIOVX_DELAY,
-#if defined(SOC_J721E) || defined(SOC_J721S2) || defined(SOC_J784S4)
-    TIOVX_DISPLAY,
-    TIOVX_CAPTURE,
-    TIOVX_AEWB,
-    TIOVX_SDE,
-    TIOVX_SDE_VIZ,
-    TIOVX_DOF,
-    TIOVX_DOF_VIZ,
-#endif
-    TIOVX_MODULES_NUM_MODULES,
-} NODE_TYPES;
-
-#ifdef __cplusplus
+void tiovx_delay_init_cfg(TIOVXDelayNodeCfg *node_cfg)
+{
+    node_cfg->src_pad = NULL;
+    node_cfg->sink_pad = NULL;
+    node_cfg->delay = 2;
 }
-#endif
 
-#endif //_TIOVX_MODULES_CBS
+vx_status tiovx_delay_init_node(NodeObj *node)
+{
+    vx_status status = VX_FAILURE;
+    TIOVXDelayNodeCfg *node_cfg = (TIOVXDelayNodeCfg *)node->node_cfg;
+    TIOVXDelayNodePriv *node_priv = (TIOVXDelayNodePriv *)node->node_priv;
+
+    node_priv->delay_obj = vxCreateDelay(
+                                  node->graph->tiovx_context,
+                                  (vx_reference)node_cfg->src_pad->exemplar_arr,
+                                  node_cfg->delay);
+
+    node->num_inputs = 1;
+    node->num_outputs = 1;
+
+    node->sinks[0].node = node;
+    node->sinks[0].pad_index = 0;
+    node->sinks[0].node_parameter_index =
+                                    node_cfg->src_pad->node_parameter_index;
+    node->sinks[0].num_channels = node_cfg->src_pad->num_channels;
+    node->sinks[0].exemplar_arr = (vx_object_array)vxGetReferenceFromDelay(
+                                                            node_priv->delay_obj,
+                                                            1 - node_cfg->delay);
+    vxRetainReference((vx_reference)node->sinks[0].exemplar_arr);
+    node->sinks[0].exemplar = vxGetObjectArrayItem(node->sinks[0].exemplar_arr,
+                                                   0);
+
+    node->srcs[0].node = node;
+    node->srcs[0].pad_index = 0;
+    node->srcs[0].node_parameter_index = node_cfg->src_pad->node_parameter_index;
+    node->srcs[0].num_channels = node_cfg->src_pad->num_channels;
+    vxRetainReference(node->sinks[0].exemplar);
+    vxRetainReference((vx_reference)node->sinks[0].exemplar_arr);
+    node->srcs[0].exemplar = node->sinks[0].exemplar;
+    node->srcs[0].exemplar_arr = node->sinks[0].exemplar_arr;
+
+    status = tiovx_modules_link_pads(node_cfg->src_pad, &node->sinks[0]);
+    if (VX_SUCCESS != status) {
+        TIOVX_MODULE_ERROR("[DELAY] Failed to link sink pad\n");
+        return status;
+    }
+
+    status = tiovx_modules_link_pads(&node->srcs[0], node_cfg->sink_pad);
+    if (VX_SUCCESS != status) {
+        TIOVX_MODULE_ERROR("[DELAY] Failed to link src pad\n");
+        return status;
+    }
+
+    return status;
+}
+
+vx_status tiovx_delay_create_node(NodeObj *node)
+{
+    vx_status status = VX_SUCCESS;
+    TIOVXDelayNodeCfg *node_cfg = (TIOVXDelayNodeCfg *)node->node_cfg;
+
+    node->tiovx_node = node_cfg->src_pad->node->tiovx_node;
+
+    return status;
+}
+
+vx_status tiovx_delay_delete_node(NodeObj *node)
+{
+    vx_status status = VX_SUCCESS;
+    TIOVXDelayNodePriv *node_priv = (TIOVXDelayNodePriv *)node->node_priv;
+
+    status = vxReleaseDelay(&node_priv->delay_obj);
+
+    return status;
+}
+
+vx_uint32 tiovx_delay_get_cfg_size()
+{
+    return sizeof(TIOVXDelayNodeCfg);
+}
+
+vx_uint32 tiovx_delay_get_priv_size()
+{
+    return sizeof(TIOVXDelayNodePriv);
+}
