274157dfafecb2d3c6855d0b597406e895839597
Rahul T R <r-ravikumar@ti.com>
2024-02-28 20:30:46 +0530

    modules: Add delay module
    Signed-off-by: Rahul T R <r-ravikumar@ti.com>

--- modules/core/src/tiovx_modules_cbs.c
+++ modules/core/src/tiovx_modules_cbs.c
@@ -158,6 +158,14 @@ NodeCbs gNodeCbs[TIOVX_MODULES_NUM_MODULES] =
         .delete_node = tiovx_modules_release_node,
         .get_cfg_size = tiovx_pyramid_get_cfg_size,
         .get_priv_size = NULL
+    },
+    {
+        .init_node = tiovx_delay_init_node,
+        .create_node = tiovx_delay_create_node,
+        .post_verify_graph = NULL,
+        .delete_node = tiovx_delay_delete_node,
+        .get_cfg_size = tiovx_delay_get_cfg_size,
+        .get_priv_size = tiovx_delay_get_priv_size
 #if defined(SOC_J721E) || defined(SOC_J721S2) || defined(SOC_J784S4)
     },
     {
