de70c03df1c4ba8c05b7290fdcc4fdd83588cb7f
Rahul T R <r-ravikumar@ti.com>
2024-03-12 13:04:12 +0530

    docs: Add doxygen config to generate api documentation
    Signed-off-by: Rahul T R <r-ravikumar@ti.com>

---
+++ docs/content_pages/doxygen.h
@@ -0,0 +1,8 @@
+
+/**
+ * \defgroup tiovx_modules EdgeAI TIOVX Apps core Interface
+ *
+ * \brief Thin layer on top of OpenVX, for easily contruct complex pipelines using OpenVX
+ *
+ * \ingroup tiovx_modules
+ */
