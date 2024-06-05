79f3edd539097469d02f53dad10d4e1df976f272
Rahul T R <r-ravikumar@ti.com>
2024-06-05 17:45:15 +0530

    tests: Add test for pixelwise add module
    Signed-off-by: Rahul T R <r-ravikumar@ti.com>

--- tests/main.c
+++ tests/main.c
@@ -93,6 +93,7 @@
 #define APP_MODULES_TEST_LINUX_CAPTURE_DOF (0)
 #define APP_MODULES_TEST_LINUX_DECODE_SDE (0)
 #define APP_MODULES_TEST_PIXELWISE_MULTIPLY (1)
+#define APP_MODULES_TEST_PIXELWISE_ADD (1)
 
 char *EDGEAI_DATA_PATH;
 
@@ -275,6 +276,16 @@ int main(int argc, char *argv[])
         int app_modules_pixelwise_multiply_test(int argc, char* argv[]);
 
         status = app_modules_pixelwise_multiply_test(argc, argv);
+    }
+#endif
+
+#if (APP_MODULES_TEST_PIXELWISE_ADD)
+    if(status==0)
+    {
+        printf("Running Pixelwise Add module test\n");
+        int app_modules_pixelwise_add_test(int argc, char* argv[]);
+
+        status = app_modules_pixelwise_add_test(argc, argv);
     }
 #endif
 
