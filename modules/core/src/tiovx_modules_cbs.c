20944b41fc15c97cb2c0a3f50f224d7ca6e02d12
Rahul T R <r-ravikumar@ti.com>
2024-06-06 00:46:43 +0530

    modules: Add module for LUT
    Signed-off-by: Rahul T R <r-ravikumar@ti.com>

--- modules/core/src/tiovx_modules_cbs.c
+++ modules/core/src/tiovx_modules_cbs.c
@@ -190,6 +190,14 @@ NodeCbs gNodeCbs[TIOVX_MODULES_NUM_MODULES] =
         .delete_node = tiovx_modules_release_node,
         .get_cfg_size = tiovx_pixelwise_add_get_cfg_size,
         .get_priv_size = NULL
+    },
+    {
+        .init_node = tiovx_lut_init_node,
+        .create_node = tiovx_lut_create_node,
+        .post_verify_graph = NULL,
+        .delete_node = tiovx_lut_delete_node,
+        .get_cfg_size = tiovx_lut_get_cfg_size,
+        .get_priv_size = tiovx_lut_get_priv_size
 #if defined(SOC_J721E) || defined(SOC_J721S2) || defined(SOC_J784S4) || defined(SOC_J722S)
     },
     {
