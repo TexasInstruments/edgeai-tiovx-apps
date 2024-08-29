d72480704d919021fddbd279fd68f668714f16d9
Shreyash Sinha <s-sinha@ti.com>
2024-08-29 15:40:10 +0530

    tests/app_tiovx_linux_capture_display_test.c: fixed VISS_DCC Path
    Signed-off-by: Shreyash Sinha <s-sinha@ti.com>

--- tests/app_tiovx_linux_capture_display_test.c
+++ tests/app_tiovx_linux_capture_display_test.c
@@ -74,7 +74,7 @@
 #define INPUT_HEIGHT (1080)
 
 #define SENSOR_NAME "SENSOR_SONY_IMX219_RPI"
-#define DCC_VISS "/imx219/linear/dcc_viss.bin"
+#define DCC_VISS "/opt/imaging/imx219/linear/dcc_viss.bin"
 
 vx_status app_modules_linux_capture_display_test(vx_int32 argc, vx_char* argv[])
 {
